bed1323920e3fec2f82b1fc8d210fa700e2aeeba
phongtranv <tranvanphong08092005@gmail.com>
2025-12-21 01:01:10 +0700

    Init Flutter project with login and static home screen
    git rebase --continue
    	pubspec.yaml

---
+++ lib/View/Login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Login'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) =>
+                    const MyHomePage(title: 'Home Page'),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
