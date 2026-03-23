ff308d3bca1fc639b835a85cf971221817be8432
Phan Lưu PHong <50897010+PhanPhong13112005@users.noreply.github.com>
2026-03-23 13:22:44 +0700

    Delete Mobile directory

--- Mobile/magic_english/android/app/src/main/kotlin/com/example/magic_english/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.magic_english
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Mobile/magic_english/lib/widgets/index.dart
+++
@@ -1,3 +0,0 @@
-// Widget exports
-export 'streak_card.dart';
-export 'stat_card.dart';
