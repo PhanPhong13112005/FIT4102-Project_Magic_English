d7b329e1848a60f6c9316eb67a03bba703bc28c3
PhanPhong13112005 <luuphongphan12@gmail.com>
2026-03-23 23:58:15 +0700

    chore: save local changes in api_constants before pull

--- Frontend/lib/core/constants/api_constants.dart
+++ Frontend/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   // Thay đổi dòng này thành domain thật của bạn
   // static const String baseUrl = "http://10.0.2.2:5000/api"; // Cũ
-  static const String baseUrl = "https://hoctienganh.ddns.net/api"; // Mới (Production)
+  static const String baseUrl = "http://magicenglish.ddns.net/api"; // Mới (Production)
 }
