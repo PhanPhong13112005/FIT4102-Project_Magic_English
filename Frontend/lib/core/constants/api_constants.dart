5b982cbb4cf00da785c2548de2314412ae3bb33c
PhanPhong13112005 <luuphongphan12@gmail.com>
2026-03-24 18:36:12 +0700

    [Update] docker-compose.yml

--- Frontend/lib/core/constants/api_constants.dart
+++ Frontend/lib/core/constants/api_constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter/foundation.dart';
 class ApiConstants {
   // Thay đổi dòng này thành domain thật của bạn
   // static const String baseUrl = "http://10.0.2.2:5000/api"; // Cũ
-  static const String baseUrl = "http://magicenglish.ddns.net/api"; // Mới (Production)
+  static const String baseUrl = "http://103.163.215.121:5181/api"; // Mới (Production)
 }
